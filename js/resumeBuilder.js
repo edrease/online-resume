852c6fd4b736820aa6991766b33fa4b547687f3a
Edrease Peshtaz <edreasepeshtaz@Edreases-Mac-mini.local>
2017-08-29 14:31:26 -0700

    fixed more errors

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,9 @@
+var data = "%data%";
+
 var bio = {
     name: "Edrease Peshtaz",
     role: "Front End Web Developer",
-    contact: {
+    contacts: {
         "mobile": "253-691-7366",
         "email": "edrease@gmail.com",
         "github": "github.com/edrease",
@@ -10,43 +12,37 @@ var bio = {
     },
     welcomeMessage: "Welcome to my resume page!",
     skills: ["iOS", "front end web development"],
-    bioPic: "images/edrease.jpeg",
+    biopic: "images/edrease.jpeg",
 
     display: function() {
-        var formattedRole = HTMLheaderRole.replace("%data%", this.role);
+        var formattedRole = HTMLheaderRole.replace(data, this.role);
         $('#header').prepend(formattedRole);
-        var formattedName = HTMLheaderName.replace("%data%", this.name);
+        var formattedName = HTMLheaderName.replace(data, this.name);
         $('#header').prepend(formattedName);
 
         //Contact Info
-        var formattedMobile = HTMLmobile.replace("%data%", this.contact.mobile);
-        $('#topContacts').append(formattedMobile);
-        var formattedEmail = HTMLemail.replace("%data%", this.contact.email);
-        $('#topContacts').append(formattedEmail);
-        var formattedGithub = HTMLgithub.replace("%data%", this.contact.github);
-        $('#topContacts').append(formattedGithub);
-        var formattedTwitter = HTMLtwitter.replace("%data%", this.contact.twitter);
-        $('#topContacts').append(formattedTwitter);
-        var formattedLocation = HTMLlocation.replace("%data%", this.contact.location);
-        $('#topContacts').append(formattedLocation);
+        var formattedMobile = HTMLmobile.replace(data, this.contacts.mobile);
+        $('#topContacts, #footerContacts').append(formattedMobile);
+        var formattedEmail = HTMLemail.replace(data, this.contacts.email);
+        $('#topContacts, #footerContacts').append(formattedEmail);
+        var formattedGithub = HTMLgithub.replace(data, this.contacts.github);
+        $('#topContacts, #footerContacts').append(formattedGithub);
+        var formattedTwitter = HTMLtwitter.replace(data, this.contacts.twitter);
+        $('#topContacts, #footerContacts').append(formattedTwitter);
+        var formattedLocation = HTMLlocation.replace(data, this.contacts.location);
+        $('#topContacts, #footerContacts').append(formattedLocation);
 
-        $('#footerContacts').append(formattedMobile);
-        $('#footerContacts').append(formattedEmail);
-        $('#footerContacts').append(formattedGithub);
-        $('#footerContacts').append(formattedTwitter);
-        $('#footerContacts').append(formattedLocation);
-
-        var formattedMessage = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
+        var formattedMessage = HTMLwelcomeMsg.replace(data, this.welcomeMessage);
         $('#header').append(formattedMessage);
 
-        var pic = HTMLbioPic.replace("%data%", this.bioPic);
+        var pic = HTMLbioPic.replace(data, this.biopic);
         $('#header').append(pic);
 
         var skillHeader = HTMLskillsStart;
         $('#header').append(skillHeader);
 
         this.skills.forEach(function(skill) {
-            var formattedSkill = HTMLskills.replace("%data%", skill);
+            var formattedSkill = HTMLskills.replace(data, skill);
             $('#header').append(formattedSkill);
         });
     }
@@ -71,7 +67,7 @@ var education = {
         }
     ],
     onlineCourses: [{
-        name: "Front End Web Developer Nanodegree",
+        title: "Front End Web Developer Nanodegree",
         school: "Udacity",
         dates: "August 2017 - September 2017",
         url: "http://www.udacity.com"
@@ -82,17 +78,17 @@ var education = {
         $('#education').append(schoolStart);
 
         this.schools.forEach(function(school) {
-            var formattedName = HTMLschoolName.replace('%data%', school.name);
+            var formattedName = HTMLschoolName.replace(data, school.name);
             formattedName = formattedName.replace('#', school.url);
-            var formattedDegree = HTMLschoolDegree.replace('%data%', school.degree);
+            var formattedDegree = HTMLschoolDegree.replace(data, school.degree);
             formattedName += formattedDegree;
             $('#education').append(formattedName);
-            var formattedLocation = HTMLschoolLocation.replace('%data%', school.location);
+            var formattedLocation = HTMLschoolLocation.replace(data, school.location);
             $('#education').append(formattedLocation);
-            var formattedDates = HTMLschoolDates.replace('%data%', school.dates);
+            var formattedDates = HTMLschoolDates.replace(data, school.dates);
             $('#education').append(formattedDates);
             //$('#education').append(formattedDegree);
-            var formattedMajor = HTMLschoolMajor.replace('%data%', school.majors[0]);
+            var formattedMajor = HTMLschoolMajor.replace(data, school.majors[0]);
             $('#education').append(formattedMajor);
 
         });
@@ -101,14 +97,14 @@ var education = {
         $('#education').append(onlineClassHeader);
 
         this.onlineCourses.forEach(function(onlineCourse) {
-            var formattedSchoolName = HTMLonlineSchool.replace('%data%', onlineCourse.school);
-            var formattedName = HTMLonlineTitle.replace('%data%', onlineCourse.name);
-            formattedName = formattedName.replace('#', onlineCourse.url);
-            formattedName += formattedSchoolName;
-            $('#education').append(formattedName);
-            var formattedDates = HTMLonlineDates.replace('%data%', onlineCourse.dates);
+            var formattedSchoolTitle = HTMLonlineSchool.replace(data, onlineCourse.school);
+            var formattedTitle = HTMLonlineTitle.replace(data, onlineCourse.title);
+            formattedTitle = formattedTitle.replace('#', onlineCourse.url);
+            formattedTitle += formattedSchoolTitle;
+            $('#education').append(formattedTitle);
+            var formattedDates = HTMLonlineDates.replace(data, onlineCourse.dates);
             $('#education').append(formattedDates);
-            var courseURL = HTMLonlineURL.replace('%data%', onlineCourse.url);
+            var courseURL = HTMLonlineURL.replace(data, onlineCourse.url);
             $('#education').append(courseURL);
         });
     }
@@ -129,15 +125,15 @@ var work = {
         var start = HTMLworkStart;
         $('#workExperience').append(start);
         this.jobs.forEach(function(job) {
-            var employer = HTMLworkEmployer.replace('%data%', job.employer);
-            var title = HTMLworkTitle.replace('%data%', job.title);
+            var employer = HTMLworkEmployer.replace(data, job.employer);
+            var title = HTMLworkTitle.replace(data, job.title);
             employer += title;
             $('#workExperience').append(employer);
-            var location = HTMLworkLocation.replace('%data%', job.location);
+            var location = HTMLworkLocation.replace(data, job.location);
             $('#workExperience').append(location);
-            var dates = HTMLworkDates.replace('%data%', job.dates);
+            var dates = HTMLworkDates.replace(data, job.dates);
             $('#workExperience').append(dates);
-            var description = HTMLworkDescription.replace('%data%', job.description);
+            var description = HTMLworkDescription.replace(data, job.description);
             $('#workExperience').append(description);
         });
     }
@@ -148,19 +144,19 @@ var projects = {
             title: "Untitled Game",
             dates: "August 2016 - Present",
             description: "A spy themed text adventure built only using the native iOS UI framework, UIKit.",
-            images: []
+            images: ["images/textadv.png"]
         },
         {
             title: "Sketch &amp; Guess",
             dates: "September 2015 - February 2016",
             description: "A local party game that mixes the classic games of 'telephone' and Pictionary.",
-            images: []
+            images: ["images/sketch.jpeg"]
         },
         {
             title: "Oblique Strategies",
             dates: "May 2015",
             description: "A digitalized deck of cards developed by artists used for breaking creative blocks.",
-            images: []
+            images: ["images/oblique.jpeg"]
         }
     ],
 
@@ -169,13 +165,14 @@ var projects = {
         var projectStart = HTMLprojectStart;
         $('#projects').append(projectStart);
         this.projects.forEach(function(project) {
-            var title = HTMLprojectTitle.replace('%data%', project.title);
+            var title = HTMLprojectTitle.replace(data, project.title);
             $('#projects').append(title);
-            var dates = HTMLprojectDates.replace('%data%', project.dates);
+            var dates = HTMLprojectDates.replace(data, project.dates);
             $('#projects').append(dates);
-            var description = HTMLprojectDescription.replace('%data%', project.description);
+            var description = HTMLprojectDescription.replace(data, project.description);
             $('#projects').append(description);
-            //TODO: add images
+            var image = HTMLprojectImage.replace(data, project.images[0]);
+            $('#projects').append(image);
         });
     }
 };
